15ac18dfd4b693e8bb38856b9c6f33d054cc439f
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-04-14 15:09:17 -0500

    location update

--- server-to-json.py
+++ server-to-json.py
@@ -2,8 +2,9 @@ import json
 import sys
 from os import chdir
 
-chdir("d:\\Users\\Geovanni\\Sync\\Projects\\UMKC-Hackathon-2018\\")
+chdir("~/UMKC-Hackathon-2018")
 
+#with open(sys.argv[1]) as server_list:
 with open("servers.txt") as server_list:
     # servers = server_list.readlines()
     servers = json.load(server_list)
@@ -11,4 +12,3 @@ with open("servers.txt") as server_list:
 print(servers['mail'])
 print(len(sys.argv))
 print(sys.argv[0])
-#print(json.dumps(servers, indent=2))
