0b45e7c3af107154f141bbb5cf3cc00456ecbaec
GeoSnipes <geovanni.fortisyute@gmail.com>
2018-04-15 15:21:16 -0500

    update ssh location

--- server-to-json.py
+++ server-to-json.py
@@ -7,7 +7,7 @@ with open("servers.txt") as server_list:
 print(json.dumps(servers, indent=4))
 
 for node in servers['mail']:
-    ssh_string = "ssh geoWe@"+node+" -i ~/.ssh/clab_rsa 'bash -s' < "
+    ssh_string = "ssh geoWe@"+node+" -i /home/geo/.jenkins/workspace/testcloudlab/clab_rsa 'bash -s' < "
     stript_string = "~/UMKC-Hackathon-2018/test.sh"
     pipe_string = " >> 'stdout_"+node+"_'$(date +'%m-%d-%y') 2>> 'stderr_"+node+"_'$(date +'%m-%d-%y')"
     os.system(ssh_string + stript_string + pipe_string)
